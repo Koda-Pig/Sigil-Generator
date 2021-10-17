04946d3e47b00a83314932e7e77abe69cb641f62
Josh Koter <josh.koter@gmail.com>
2021-10-17 21:46:32 +0200

    Refactored and added some style

--- main.js
+++ main.js
@@ -4,56 +4,65 @@ import './style.css'
 const themeSwitchButton = document.getElementById('themeSwitchButton')
 themeSwitchButton.addEventListener('click', switchTheme);
 function switchTheme() {
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle('dark-mode');
 }
 
 
 // sigil generator
-const submitButton = document.getElementById('submit');
+const genSigilBtn = document.getElementById('genSigilBtn');
 let output = document.getElementById('output');
-submitButton.addEventListener('click', genSigil);
+let description = document.getElementById('description');
+genSigilBtn.addEventListener('click', genSigil);
 
 
 function genSigil() {
+    
 
-    function removeDuplicate(userInput, n) {    // this works
-        var index = 0;
-        for (var i = 0; i < n; i++) {
-            var j;
-            for (j = 0; j < i; j++) {
-                if (userInput[i] == userInput[j]) {
-                    break;
+    let userInput = document.getElementById('input').value.split('');
+
+    if (userInput == undefined || userInput == '') {
+        alert(`You didn't enter anything in`);
+    } else {
+        let n = userInput.length;
+        let removedDuplicates = removeDuplicate(userInput, n);
+        let finalString;
+        function removeDuplicate(userInput, n) {
+            let index = 0;
+            for (let i = 0; i < n; i++) {
+                let j;
+                for (j = 0; j < i; j++) {
+                    if (userInput[i] == userInput[j]) {
+                        break;
+                    }
+                }
+                if (j == i) {
+                    userInput[index++] = userInput[i];
                 }
             }
-            if (j == i) {
-                userInput[index++] = userInput[i];
-            }
+            return userInput.join("").slice(userInput, index);
         }
-        return userInput.join("").slice(userInput, index);
-    }
-    var userInput = document.getElementById('input').value.split('');
-    var n = userInput.length;
-    var removedDuplicates = removeDuplicate(userInput, n);
-    var finalString = removedDuplicates;
+        
 
 
-    function removeDuplicates(input) {
-        let disallow = [' ', 'a', 'e', 'i', 'o', 'u'];
-        let stringFromArray = input.split('');
 
-        for (let i = 0; i < disallow.length; i++) {
-            for (let j = 0; j < stringFromArray.length; j++) {
-                if (disallow[i] == stringFromArray[j]) {
-                    stringFromArray.splice(j,1);
+        function removeDuplicates(input) {
+            let disallow = [' ', 'a', 'e', 'i', 'o', 'u'];
+            let stringFromArray = input.split('');
+
+            for (let i = 0; i < disallow.length; i++) {
+                for (let j = 0; j < stringFromArray.length; j++) {
+                    if (disallow[i] == stringFromArray[j]) {
+                        stringFromArray.splice(j,1);
+                    }
                 }
             }
+            finalString = stringFromArray.join('');
         }
-        finalString = stringFromArray.join('');
+        removeDuplicates(removedDuplicates);
+
+        output.innerHTML = finalString;
+        description.innerHTML = `removed duplicate characters, spaces, and vowels.`
     }
-    removeDuplicates(removedDuplicates);
-
-
-
-    output.innerHTML = finalString;
+    
 }
